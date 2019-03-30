d0236830ba225b74ca7c83c82672398c5df149a6
Saurabh Chaturvedi <saurabh.chaturvedi63@gmail.com>
2019-03-31 03:18:20 +0530

    GitHub Data + Watson APIs

--- githavior/analyzer.py
+++ githavior/analyzer.py
@@ -1,11 +1,19 @@
 """
 Analyzes user data using IBM Watson Personality Insights
 and Natural Language Understanding APIs.
+
+Usage:
+  analyzer.py <user> [-r]
+  analyzer.py -h
 """
+
 import json
+from docopt import docopt
 from ibm_watson import PersonalityInsightsV3, NaturalLanguageUnderstandingV1
 from ibm_watson.natural_language_understanding_v1 import Features, ConceptsOptions, EntitiesOptions, KeywordsOptions
 from credentials import NLU_KEY, PERSONALITY_INSIGHTS_KEY
+
+from fetch_repos import get_commits, get_pr_issues_body
 
 personality_service = PersonalityInsightsV3(
     version='2017-10-13',
@@ -13,29 +21,32 @@ personality_service = PersonalityInsightsV3(
     username='apikey',
     password=PERSONALITY_INSIGHTS_KEY)
 
-with open('profile.json') as profile_json:
-    profile = personality_service.profile(
-        profile_json.read(),  # behavior not returned even for temporal data!
-        'application/json',
-        raw_scores=True,
-        consumption_preferences=True).get_result()
-print(json.dumps(profile, indent=4))
-
 naturalLanguageUnderstanding = NaturalLanguageUnderstandingV1(
     version='2018-11-16',
     url='https://gateway-lon.watsonplatform.net/natural-language-understanding/api',
     username='apikey',
     password=NLU_KEY)
 
-nlu_response = naturalLanguageUnderstanding.analyze(
-    text='IBM is an American multinational technology company '
-    'headquartered in Armonk, New York, United States, '
-    'with operations in over 170 countries.',
-    features=Features(
-        entities=EntitiesOptions(emotion=True, sentiment=True, limit=5),
-        keywords=KeywordsOptions(emotion=True, sentiment=True),
-        concepts=ConceptsOptions(limit=5)
-        )
-    ).get_result()
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+    author = arguments['<user>']
 
-print(json.dumps(nlu_response, indent=4))
+    profile = personality_service.profile(
+        str(get_commits(author)),  # behavior not returned even for temporal data!
+        'application/json',
+        raw_scores=True,
+        consumption_preferences=True).get_result()
+    body = get_pr_issues_body(author)
+
+    nlu_response = naturalLanguageUnderstanding.analyze(
+        text=' '.join(body),
+        features=Features(
+            entities=EntitiesOptions(emotion=True, sentiment=True, limit=5),
+            keywords=KeywordsOptions(emotion=True, sentiment=True),
+            concepts=ConceptsOptions(limit=5)
+            )
+        ).get_result()
+
+    print(json.dumps(profile, indent=4))
+    print('-*-'*33)
+    print(json.dumps(nlu_response, indent=4))
