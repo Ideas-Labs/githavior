b83598ace17cf7fb860ff18b6ce5cbb90a499c1f
Saurabh Chaturvedi <saurabh.chaturvedi63@gmail.com>
2019-03-31 04:01:31 +0530

    Handle NoneType error

--- githavior/analyzer.py
+++ githavior/analyzer.py
@@ -8,12 +8,24 @@ Usage:
 """
 
 import json
+import os
+
+import flask
+
 from docopt import docopt
 from ibm_watson import PersonalityInsightsV3, NaturalLanguageUnderstandingV1
 from ibm_watson.natural_language_understanding_v1 import Features, ConceptsOptions, EntitiesOptions, KeywordsOptions
-from credentials import NLU_KEY, PERSONALITY_INSIGHTS_KEY
+
+try:
+    from credentials import NLU_KEY, PERSONALITY_INSIGHTS_KEY
+except ModuleNotFoundError:  # For Heroku
+    NLU_KEY = os.environ['NLU_KEY']
+    PERSONALITY_INSIGHTS_KEY = os.environ['PERSONALITY_INSIGHTS_KEY']
 
 from fetch_repos import get_commits, get_pr_issues_body
+
+app = flask.Flask(__name__)
+
 
 personality_service = PersonalityInsightsV3(
     version='2017-10-13',
@@ -30,16 +42,30 @@ naturalLanguageUnderstanding = NaturalLanguageUnderstandingV1(
 if __name__ == '__main__':
     arguments = docopt(__doc__)
     author = arguments['<user>']
+    
+    commits = get_commits(author)
+    body = get_pr_issues_body(author)
+
+    body_list = [{
+         "content": text,
+         "contenttype": "text/plain",
+         "language": "en"
+      } for text in body
+      ]
+    
+    aug_content_list = commits.copy()
+    aug_content_list["contentItems"] += body_list
+    
+    aug_body = body + [comm["content"] for comm in commits["contentItems"]]
 
     profile = personality_service.profile(
-        str(get_commits(author)),  # behavior not returned even for temporal data!
+        str(aug_content_list),  # behavior not returned even for temporal data!
         'application/json',
         raw_scores=True,
         consumption_preferences=True).get_result()
-    body = get_pr_issues_body(author)
 
     nlu_response = naturalLanguageUnderstanding.analyze(
-        text=' '.join(body),
+        text=' '.join(aug_body),
         features=Features(
             entities=EntitiesOptions(emotion=True, sentiment=True, limit=5),
             keywords=KeywordsOptions(emotion=True, sentiment=True),
